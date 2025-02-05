e72ac6a5bcdd93d2b37995c171816bbc55a201d8
Muhammad Mubashir Ali <alimuhammad98573@gmail.com>
2025-02-06 00:52:58 +0500

    e-commerce-website-3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 // import Head from "next/head";
 
 const geistSans = Geist({
@@ -28,9 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header/>
+        <Header/>
         {children}
-        <footer/>
+        <Footer/>
       </body>
     </html>
   );
